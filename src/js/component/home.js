403ecdeff597cae26052ffd31c98842891fdaee5
achaves100 <adan.chaves@outlook.com>
2021-03-30 03:55:15 +0000

    Updated Click and Change

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,26 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [count, setCount] = useState(0);
+	const [inputValue, setInputValue] = useState("Fernando");
+
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+	const handleChange = e => {
+		setInputValue(e.target.value);
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>You clicked {count} times!</h1>
+			<h1>Hello {inputValue} from React State!</h1>
+			<button className="btn btn-primary" onClick={handleClick}>
+				{" "}
+				Click Here
+			</button>
+			<input onChange={event => handleChange(event)} />
 		</div>
 	);
 }
